775915598148dbd85b691f270b15e70036e32540
João Lopes <joaomarcoslopes@id.uff.br>
2020-09-30 22:52:58 -0300

    chore: spring setup

---
+++ src/main/java/com/pluralsight/productapiannotation/ProductApiAnnotationApplication.java
@@ -0,0 +1,29 @@
+package com.pluralsight.productapiannotation;
+
+import com.pluralsight.productapiannotation.model.Product;
+import com.pluralsight.productapiannotation.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class ProductApiAnnotationApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApiAnnotationApplication.class, args);
+    }
+    
+    @Bean
+    CommandLineRunner init(ProductRepository productRepository) {
+        return args -> {
+            Flux.just(new Product(null, "Big Latte", 2.99),
+                      new Product(null, "Big Decaf", 2.49),
+                      new Product(null, "Green Tea", 1.99))
+                    .flatMap(productRepository::save)
+                    .subscribe();
+        };
+    }
+    
+}
---
+++ src/test/java/com/pluralsight/productapiannotation/ProductApiAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.productapiannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiAnnotationApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
